0a97274f90434c94feb0a5ea452284df31c3d7e5
avivcz <avivcz@users.noreply.github.com>
2025-11-18 15:53:00 +0200

    wip

--- playground/rgb.js
+++ playground/rgb.js
@@ -1,63 +1,17 @@
-import rpio from "rpio";
+import rpio from 'rpio';
 
-// your confirmed physical pins:
-const RED   = 32;  // GPIO12  (PWM0)
-const GREEN = 33;  // GPIO13  (PWM1)
-const BLUE  = 12;  // GPIO18  (PWM0)
+rpio.init({ gpiomem: false });   // default mapping = physical
 
-const RANGE = 255;
+const RED   = 32;   // what you THINK is red
+const GREEN = 33;   // what you THINK is green
+const BLUE  = 12;   // what you THINK is blue
 
-// common-anode: low = ON, high = OFF
-const ON  = 0;
-const OFF = RANGE - 1;  // 254, not 255
+const pins = { RED, GREEN, BLUE };
 
-function sleep(ms) {
-  return new Promise((res) => setTimeout(res, ms));
+for (const [name, pin] of Object.entries(pins)) {
+  console.log("Testing", name, "on pin", pin);
+  rpio.open(pin, rpio.OUTPUT, rpio.HIGH);
+  rpio.write(pin, rpio.LOW);   // turn ON (common anode)
+  await new Promise(r => setTimeout(r, 2000));
+  rpio.write(pin, rpio.HIGH);  // turn OFF
 }
-
-function initPins() {
-  rpio.init({ mapping: "physical", gpiomem: false });
-
-  rpio.open(RED, rpio.PWM);
-  rpio.open(GREEN, rpio.PWM);
-  rpio.open(BLUE, rpio.PWM);
-
-  rpio.pwmSetRange(RED, RANGE);
-  rpio.pwmSetRange(GREEN, RANGE);
-  rpio.pwmSetRange(BLUE, RANGE);
-
-  rpio.pwmSetClockDivider(64); // power of 2, global
-}
-
-function setColor(r, g, b) {
-  rpio.pwmSetData(RED, r);
-  rpio.pwmSetData(GREEN, g);
-  rpio.pwmSetData(BLUE, b);
-}
-
-async function showColor(name, r, g, b) {
-  console.log(`Showing ${name}`, { r, g, b });
-  setColor(r, g, b);
-  await sleep(1500);
-}
-
-function cleanup() {
-  console.log("Cleaning up…");
-  setColor(OFF, OFF, OFF); // everything OFF
-  process.exit(0);
-}
-
-async function main() {
-  initPins();
-
-  process.on("SIGINT", cleanup);
-  process.on("SIGTERM", cleanup);
-
-  while (true) {
-    await showColor("RED",   ON,  OFF, OFF);
-    await showColor("GREEN", OFF, ON,  OFF);
-    await showColor("BLUE",  OFF, OFF, ON);
-  }
-}
-
-main();
