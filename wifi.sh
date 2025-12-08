90fcf4efdc679129e53a0df5fecd05db49de623f
avivcz <avivcz@users.noreply.github.com>
2025-12-08 13:33:55 +0200

    wppp

--- wifi.sh
+++ wifi.sh
@@ -1,7 +1,7 @@
-sudo nmcli connection modify "NETGEAR67-5G" ipv4.addresses 10.0.0.50/24
-sudo nmcli connection modify "NETGEAR67-5G" ipv4.gateway 10.0.0.1
-sudo nmcli connection modify "NETGEAR67-5G" ipv4.dns "10.0.0.1 8.8.8.8"
-sudo nmcli connection modify "NETGEAR67-5G" ipv4.method manual
+sudo nmcli connection modify "NETGEAR67-5G 1" ipv4.addresses 10.0.0.50/24
+sudo nmcli connection modify "NETGEAR67-5G 1" ipv4.gateway 10.0.0.1
+sudo nmcli connection modify "NETGEAR67-5G 1" ipv4.dns "10.0.0.1 8.8.8.8"
+sudo nmcli connection modify "NETGEAR67-5G 1" ipv4.method manual
 
-sudo nmcli connection down "NETGEAR67-5G"
-sudo nmcli connection up "NETGEAR67-5G"
+sudo nmcli connection down "NETGEAR67-5G 1"
+sudo nmcli connection up "NETGEAR67-5G 1"
