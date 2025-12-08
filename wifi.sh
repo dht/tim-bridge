2a1c72cbff86e812f349067db61f37619a2314ef
avivcz <avivcz@users.noreply.github.com>
2025-12-08 12:03:34 +0200

    wip

--- wifi.sh
+++ wifi.sh
@@ -1,4 +1,4 @@
-sudo nmcli connection modify "NETGEAR67-5G" ipv4.addresses 10.0.0.51/24
+sudo nmcli connection modify "NETGEAR67-5G" ipv4.addresses 10.0.0.50/24
 sudo nmcli connection modify "NETGEAR67-5G" ipv4.gateway 10.0.0.1
 sudo nmcli connection modify "NETGEAR67-5G" ipv4.dns "10.0.0.1 8.8.8.8"
 sudo nmcli connection modify "NETGEAR67-5G" ipv4.method manual
