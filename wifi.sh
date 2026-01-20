1bdd2e01a6ad447618c38ae9950104251d8c5be7
avivcz <avivcz@users.noreply.github.com>
2026-01-20 17:17:07 +0200

    ss

--- wifi.sh
+++ wifi.sh
@@ -1,7 +1,8 @@
-sudo nmcli connection modify "NETGEAR67" ipv4.addresses 10.0.0.55/24
-sudo nmcli connection modify "NETGEAR67" ipv4.gateway 10.0.0.1
-sudo nmcli connection modify "NETGEAR67" ipv4.dns "10.0.0.1 8.8.8.8"
-sudo nmcli connection modify "NETGEAR67" ipv4.method manual
+sudo nmcli connection modify preconfigured \
+  ipv4.method manual \
+  ipv4.addresses 10.0.0.55/24 \
+  ipv4.gateway 10.0.0.1 \
+  ipv4.dns "10.0.0.1 8.8.8.8"
 
-sudo nmcli connection down "NETGEAR67"
-sudo nmcli connection up "NETGEAR67"
+sudo nmcli connection down preconfigured
+sudo nmcli connection up preconfigured
