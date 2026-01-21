4cc3b2b4e5c402047f617bdd380607f4eb5c6043
avivcz <avivcz@users.noreply.github.com>
2026-01-21 18:46:39 +0200

    wip

--- playground/matrix-pixel/blue.py
+++ playground/matrix-pixel/blue.py
@@ -5,7 +5,7 @@ from idotmatrix.connectionManager import ConnectionManager
 
 MAC_ADDRESS ="548D5EE8-4BEB-6B78-E532-6E44368D78AD"
 PI_5_ADDRESS = "F2:1A:C7:C5:53:24 IDM-C55324"
-ADDRESS = os.getenv("IDOTMATRIX_ADDRESS", PI_5_ADDRESS) )
+ADDRESS = os.getenv("IDOTMATRIX_ADDRESS", PI_5_ADDRESS)
 # macOS: usually a BLE UUID; Linux/Raspberry Pi: usually a MAC like "AA:BB:CC:DD:EE:FF"
 
 async def main():
