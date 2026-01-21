ccf4ec787c6e862ea9b6ef97f9dbb18bad142824
avivcz <avivcz@users.noreply.github.com>
2026-01-21 18:48:50 +0200

    wip

--- playground/matrix-pixel/blue.py
+++ playground/matrix-pixel/blue.py
@@ -1,16 +1,25 @@
+import sys
 import asyncio
-import os
 from idotmatrix import FullscreenColor
 from idotmatrix.connectionManager import ConnectionManager
 
-MAC_ADDRESS ="548D5EE8-4BEB-6B78-E532-6E44368D78AD"
-PI_5_ADDRESS = "F2:1A:C7:C5:53:24 IDM-C55324"
-ADDRESS = os.getenv("IDOTMATRIX_ADDRESS", PI_5_ADDRESS)
-# macOS: usually a BLE UUID; Linux/Raspberry Pi: usually a MAC like "AA:BB:CC:DD:EE:FF"
+MAC_ADDRESS = "548D5EE8-4BEB-6B78-E532-6E44368D78AD"  # macOS BLE UUID
+PI_ADDRESS  = "F2:1A:C7:C5:53:24"                   # Linux BLE MAC
+
+def get_address():
+    if sys.platform == "darwin":
+        return MAC_ADDRESS
+    elif sys.platform.startswith("linux"):
+        return PI_ADDRESS
+    else:
+        raise RuntimeError(f"Unsupported platform: {sys.platform}")
 
 async def main():
+    address = get_address()
+    print(f"Connecting to {address}")
+
     conn = ConnectionManager()
-    await conn.connectByAddress(ADDRESS)
+    await conn.connectByAddress(address)
 
     fs = FullscreenColor()
     fs.conn = conn
