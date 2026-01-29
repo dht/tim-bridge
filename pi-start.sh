01c729d0a29cbd9fbb75c1c3874091bdd5bb6230
dht <dht@users.noreply.github.com>
2026-01-29 19:33:05 +0200

    wip

--- pi-start.sh
+++ pi-start.sh
@@ -1,1 +1,7 @@
-ssh admin@10.0.0.51 "cd ~/projects/tim-bridge && pm2 start npm --name houses -- start"
+ssh admin@10.0.0.51 '
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+cd ~/projects/tim-bridge &&
+pm2 start npm --name houses -- start
+'
