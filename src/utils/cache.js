9203acf99d7ddabe59c8804bcf0b7568a23261ae
dht <dht@users.noreply.github.com>
2026-02-04 22:16:16 +0200

    wip

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -51,5 +51,5 @@ export const cacheOrder = async (order) => {
 };
 
 export const getTimelineUrl = (machineId, sessionId) => {
-  return `${STORAGE_BASE_URL}/${machineId}/sessions/${sessionId}/_timeline.json?t=7`;
+  return `${STORAGE_BASE_URL}/${machineId}/sessions/${sessionId}/_timeline.json?t=10`;
 };
