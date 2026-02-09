4daa0de9f6f04e8e00739e6b17ec3c07df1b1e11
dht <dht@users.noreply.github.com>
2026-02-09 12:02:20 +0200

    wip

---
+++ src/index.dev.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { MACHINES_DEV } from './data/data.machines.js';
+import { startMachine } from './listen.js';
+import { initFirestore } from './utils/firestore.js';
+import { initLogger } from './utils/logger.js';
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const DEV_MACHINE_ID = process.env.DEV_MACHINE_ID;
+
+export async function mainDev() {
+  initFirestore();
+  const logger = initLogger(CLIENT_ID);
+
+  await logger.clearLogs();
+
+  let machineIds = Object.values(MACHINES_DEV).map((m) => m.id);
+
+  if (DEV_MACHINE_ID) {
+    machineIds = DEV_MACHINE_ID.split(',').map((id) => id.trim());
+  }
+
+  machineIds.forEach((machineId) => {
+    startMachine(machineId);
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
 import 'dotenv/config';
-import { MACHINES_DEV } from './data/data.machines.js';
+import { mainDev } from './index.dev.js';
 import { startMachine } from './listen.js';
 import { registerCleanupHandlers } from './utils/cleanup.js';
 import { initFirestore } from './utils/firestore.js';
-import { initLogger } from './utils/logger.js';
 
 const MACHINE_ID = process.env.MACHINE_ID;
-const CLIENT_ID = process.env.CLIENT_ID;
 const IS_DEV = process.env.IS_DEV === 'true';
-const DEV_MACHINE_ID = process.env.DEV_MACHINE_ID;
 
 async function main() {
   initFirestore();
-  const logger = initLogger(CLIENT_ID);
-
-  if (IS_DEV) {
-    await logger.clearLogs();
-  }
-
-  // product or single machine dev
-  if (!IS_DEV || DEV_MACHINE_ID) {
-    // start one
-    startMachine(MACHINE_ID);
-  } else {
-    // start all
-    Object.values(MACHINES_DEV).forEach((machine) => {
-      startMachine(machine.id);
-    });
-  }
+  startMachine(MACHINE_ID);
 }
 
-main();
+if (IS_DEV) {
+  mainDev();
+} else {
+  main();
+}
+
 registerCleanupHandlers();
