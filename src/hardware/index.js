e2723740ef470b7c4d75461d67a2b19f584f09f8
avivcz <avivcz@users.noreply.github.com>
2026-02-10 12:36:57 +0200

    wip

--- src/hardware/index.js
+++ src/hardware/index.js
@@ -1,5 +1,6 @@
 import { applyHardware as applyAudio, stopAudio } from './audio.js';
 import { applyHardware as applyLights, turnLights } from './lights.js';
+import { closeBrowser, closeBrowserDelayed, openBrowser } from './browser.js';
 
 const hardwareFields = {
   mp3LocalPath: applyAudio,
@@ -24,4 +25,7 @@ export const applyHardware = (fieldId, value, meta) => {
 export const stopAllHardware = (machineId) => {
   stopAudio();
   turnLights('NONE');
+  closeBrowser();
 };
+
+export { openBrowser, closeBrowser, closeBrowserDelayed };
