abac0f8896d74872fc1981d3bb99cd247eaa7b33
avivcz <avivcz@users.noreply.github.com>
2026-02-10 13:25:10 +0200

    wip

--- src/lifecycle/A-901.js
+++ src/lifecycle/A-901.js
@@ -1,10 +1,19 @@
-export function onBridgeOpen(id, data) {}
+export function onBridgeOpen(id, data) {
+/* reach to point that:
+1. you have one browser instance
+2. with one tab
+3. which is on url about:blank
+*/
+}
 
 export function onChange(id, data) {
   const { timelineUrl, status, originWebpageUrl } = ev.data;
 }
 
-export function onBridgeClose(id, data) {}
+export function onBridgeClose(id, data) {
+  // close all instances of browser
+
+}
 
 export const lifecycle = {
   onBridgeOpen,
