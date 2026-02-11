cbe3c75a10568bf0e655c0d0a11d8bd2caf64f0d
dht <dht@users.noreply.github.com>
2026-02-11 19:06:16 +0200

    wip

--- src/hardware/index.js
+++ src/hardware/index.js
@@ -1,18 +1,24 @@
 import { applyHardware as applyAudio, stopAudio } from './audio.js';
-import { applyBrowser, closeBrowser, closeBrowserDelayed, openBrowser, openOrUpdateBrowser as openBrowserSingle } from './browser.js';
+import {
+  applyBrowser,
+  closeBrowser,
+  closeBrowserDelayed,
+  openBrowser,
+  openOrUpdateBrowser as openBrowserSingle,
+} from './browser.js';
 import { applyHardware as applyLights, turnLights } from './lights.js';
 
 const hardwareFields = {
   mp3LocalPath: applyAudio,
   lightStatus: applyLights,
-  browserUrl: applyBrowser, /*
+  browserUrl: applyBrowser /*
     1. it's empty ('') => change the only tab to about:blank
     2. it's a URL=> change the only tab to the url
 
     applyUrlToOnlyTab
 
 
-  */
+  */,
 };
 
 // meta has { machineId }
@@ -20,8 +26,6 @@ export const applyHardware = (fieldId, value, meta) => {
   if (!Object.keys(hardwareFields).includes(fieldId)) {
     return;
   }
-
-  console.log('value ->', value);
 
   const applyFunction = hardwareFields[fieldId];
 
